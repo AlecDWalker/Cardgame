7eb1620f7434e024aa505d576a00417008e6f9d4
AlecDWalker <walkeralec@hotmail.com>
2020-03-28 13:59:26 +0000

    P12 created

---
+++ lib/cards/pledges/P12.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require './card.rb'
+
+class P12 < Pledge
+  def initialize
+    @title = "It's All Lies"
+    @card_type = 'Pledge'
+    @cost = 1
+    @text = 'Remove all RUMOURS from your hand and gain 2 VOTERS for each RUMOUR removied this way'
+  end
+
+  def effect
+    rum = 0
+    current_player.hand.each { |card|
+      if card.is_a? Rumour
+        rum += 1
+        card.delete
+      end
+    }
+    end
+    current_player.gain_voters((rum * 2))
+  end
+end
